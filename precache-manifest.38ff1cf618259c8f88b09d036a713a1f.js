6367931b2b73e070d833284a8a22f968a2d00364
smriti29 <smritip29@gmail.com>
2019-08-02 19:11:32 +0530

    Updates

---
+++ precache-manifest.38ff1cf618259c8f88b09d036a713a1f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cafc11784bcda3839c3e",
+    "url": "/React_apps/authorquiz/static/js/runtime~main.7d383da6.js"
+  },
+  {
+    "revision": "9001b98c17b7d475054f",
+    "url": "/React_apps/authorquiz/static/js/main.cdea053f.chunk.js"
+  },
+  {
+    "revision": "0796282488012de28b4a",
+    "url": "/React_apps/authorquiz/static/js/2.39b06ef1.chunk.js"
+  },
+  {
+    "revision": "9001b98c17b7d475054f",
+    "url": "/React_apps/authorquiz/static/css/main.e51e68e9.chunk.css"
+  },
+  {
+    "revision": "aa7331ab35416008b7d68da5ef75e8b7",
+    "url": "/React_apps/authorquiz/index.html"
+  }
+];
